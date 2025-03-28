9f764e1f708c819b4dc7f88f074b88a4e0f9c7fb
ochealean <110779576+ochealean@users.noreply.github.com>
2025-03-29 02:04:19 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const crops = [];
+const resour = [];
 const seed_value = [0.01, 0.15, 0.5, 1, 1.5, 7, 5, 7];
 const harvested_value = [0.02, 0.24, 0.8, 1.5, 2.3, 9, 7, 10];
 const harvested_name = ["Sunflower", "Rhubarb", "Carrot", "Cabbage", "Soybean", "Corn", "Wheat", "Kale"];
@@ -37,5 +38,49 @@ calculate.addEventListener('click', () => {
     // Display results
     const result = document.getElementById('result');
 
+    console.log(total.reduce((a, b) => a + b, 0).toFixed(2));
+    if(total.reduce((a, b) => a + b, 0).toFixed(2) != 0) {
     result.innerHTML = output; // Use innerHTML to render HTML content
+    }
+});
+
+const calculate_reso = document.getElementById('calc_btn_reso');
+calculate_reso.addEventListener('click', () => {
+    let total_res = 0;
+    // Retrieve crop values from input fields
+    resour[0] = document.getElementById('wood_int').value * 20;
+    resour[1] = document.getElementById('stone_int').value * 80;
+    resour[2] = document.getElementById('iron_int').value * 480;
+    resour[3] = document.getElementById('gold_int').value * 2540;
+    resour[4] = document.getElementById('crimstone_int').value * 7780;
+    
+    // print total for each crop
+    document.getElementById('wood_total').innerHTML = resour[0];
+    document.getElementById('stone_total').innerHTML = resour[1];
+    document.getElementById('iron_total').innerHTML = resour[2];
+    document.getElementById('gold_total').innerHTML = resour[3];
+    document.getElementById('crimstone_total').innerHTML = resour[4];
+    
+    // Calculate total for each crop
+    for (let i = 0; i < resour.length; i++) {
+        total_res += parseInt(resour[i]);
+    }
+
+    // // Build output string
+    // let output = "";
+    // for (let i = 0; i < resour.length; i++) {
+    //     if (resour[i] != "0") {
+    //         output += `${harvested_name[i]}: ${total[i].toFixed(2)}<br>`; // Format to 2 decimal places
+    //     }
+    // }
+    // output += `<h4>Total: ${total.reduce((a, b) => a + b, 0).toFixed(2)}</h4>`;
+    // output += `<h4>Resources(Capital): ${total_capital.toFixed(2)}</h4>`;
+    // output += `<h4>Net Profit(Income): ${(total.reduce((a, b) => a + b, 0) - total_capital).toFixed(2)}</h4>`;
+    // Display results
+    const result = document.getElementById('result_reso');
+
+    // console.log(total.reduce((a, b) => a + b, 0).toFixed(2));
+    // if(total.reduce((a, b) => a + b, 0).toFixed(2) != 0) {
+    result.innerHTML = total_res; // Use innerHTML to render HTML content
+    // }
 });
